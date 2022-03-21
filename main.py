7f09b9d4221b7f2e625f2858e0d5d95c8a9f3872
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2022-03-21 14:15:52 +1100

    Stage 5

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 from errors_handler import *
 from pathlib import Path
-from sys import argv
+import sys
+import ast
+
 
 
 class CodeAnalyzer:
-    def __init__(self, base_path: str, error_map: dict[str, str],
-                 check_initials: list, checks: int, formatting: int = 2):
+    def __init__(self,
+                 base_path: str,
+                 check_initials: list,
+                 formatting: int = 2):
         self.base_path = base_path
-        self.error_map = error_map
+        self.tree = None
         self.lines = None
         self.errors_results = None
         self.check_initials = check_initials
-        self.checks = checks + 1
         self.formatting = formatting
 
     def open_file(self, path: str) -> None:
         with open(path, 'r') as file:
             self.lines = [line for line in file.readlines()]
+        with open(path, 'r') as file:
+            self.tree = ast.parse(file.read())
 
-    def run_checks(self, letter: str, qty: int) -> None:
-        checks = [''.join((f'check_{letter}00', str(i), '(line, line_ind, self.lines)')) for i in range(1, qty)]
-        self.errors_results = []
+
+    def run_checks1(self, letter: str) -> None:
+        checks = [''.join((f'check_{letter}00', str(i), '(line, line_ind, self.lines)')) for i in range(1, 10)]
         for line in self.lines:
             line_ind = self.lines.index(line)
             for check in checks:
                 if eval(check) and eval(check) not in self.errors_results:
                     self.errors_results.append(eval(check))
+
+    def run_checks2(self, letter: str) -> None:
+        checks = [''.join((f'check_{letter}0', str(i), '(self.tree)')) for i in range(10, 13)]
+        for check in checks:
+            if eval(check) and eval(check) not in self.errors_results:
+                self.errors_results += eval(check)
 
     def print_errs(self, path: str) -> None:
         if self.errors_results:
@@ -46,19 +57,18 @@ class CodeAnalyzer:
 
     def analyze(self) -> None:
         for path in self.get_files():
+            self.errors_results = []
             self.open_file(path)
             for letter in self.check_initials:
-                self.run_checks(letter, self.checks)
+                self.run_checks1(letter)
+                self.run_checks2(letter)
             self.print_errs(path)
 
 
-args = argv
-user_path = Path(args[1])
+args = sys.argv
 check_letters = ['s']
 
-my_analyzer = CodeAnalyzer(user_path, errors_dict,
-                           check_letters, checks=9,
-                           formatting=1)
-
 if __name__ == '__main__':
-    my_analyzer.analyze()
+    user_path = Path(input('Enter path to dir or file >')) if len(args) == 1 else Path(args[1])
+    my_analyzer = CodeAnalyzer(user_path, check_letters, formatting=1)
+    my_analyzer.analyze() 
